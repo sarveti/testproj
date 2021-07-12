6741672a71bcfb437013a540582c74fed36f2860
Sateesh A <Sateesh@HEALTHEC.COM>
2021-07-12 09:02:37 +0530

    First commit

---
+++ WebApplication5/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("visited index page.");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            _logger.LogError("Error occured in Privacy page.");
+
+            throw new NotImplementedException();
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
